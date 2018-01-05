84b5accd8ba421f05cd84a80d4f5dc2c411c87ea
fangtao04 <fangtao04@meituan.com>
2018-01-06 02:37:59 +0800

    m

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
-var fs = require('fs')
-const Koa = require('koa');
+const fs = require('fs')
+const url = require('url')
+const qs = require('qs')
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
 const { createCanvas, loadImage } = require('canvas')
 
-
-const produceImage = ({text = 'hello', width = 100, height = 100, color = '#000', bgColor = '#fff'}) => new Promise((resolve, reject) => {
+const produceImage = ({text = '', width = 500, height = 300, color = '#fff', bgColor = '#1f8dd6'}) => new Promise((resolve, reject) => {
     const canvas = createCanvas(width, height)
     const ctx = canvas.getContext('2d')
-    ctx.font = '30px Impact'
-    ctx.fillText(text, 0, 10)
+    ctx.font = '40px Impact'
+    ctx.textAlign = 'left'
+    // ctx.textBaseline = 'top'
+
+    const {width: textWidth} = ctx.measureText(text)
+
+    ctx.fillStyle = bgColor
+    ctx.fillRect(0, 0, width, height)
+    ctx.fillStyle = color
+    ctx.fillText(text, (width - textWidth) / 2, height / 2)
 
     var out = fs.createWriteStream(__dirname + '/text.png')
     var stream = canvas.pngStream()
 
     stream.on('data', function(chunk){
-      out.write(chunk);
-    });
+      out.write(chunk)
+    })
 
     stream.on('end', function(){
-      console.log('The PNG stream 结束');
       var input = fs.createReadStream(__dirname + '/text.png')
       resolve(input)
-    });
+    })
 
     out.on('finish', function(){
-      console.log('The PNG file was created.');
-    });
+      console.log('The PNG file was created.')
+    })
 })
 
 
 
 
-const app = new Koa();
+const app = new Koa()
+
+app.use(bodyParser())
 
 app.use(async ctx => {
     ctx.type = 'png'
-    // ctx.body = 'Hello World';
-    ctx.body = await produceImage({})
-});
+    ctx.body = await produceImage(qs.parse(url.parse(ctx.request.url).query))
+})
 
-app.listen(3001);
+app.listen(3001)
